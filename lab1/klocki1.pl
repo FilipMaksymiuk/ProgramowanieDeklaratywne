6dad691a83534de1e5c07e311561d6a400bd99ef
FilipMaksymiuk <115099709+FilipMaksymiuk@users.noreply.github.com>
2024-05-10 19:00:00 +0200

    Add files via upload

---
+++ lab1/klocki1.pl
@@ -0,0 +1,25 @@
+%PROGRAM: klocki_1
+%Baza wiedzy o uk³adzie klocków
+%Definiowane predykaty:
+%na/2
+%===============================
+
+
+%na(X,Y)
+%opis: spe³niony, gdy klocek x le¿y
+%bezpoœrednio na klocku Y
+%================================na
+
+na(c,a).
+na(c,b).
+na(d,c).
+%==================================na
+
+/*
+Informacje o budowie programu:
+Program sk³¹da sie z 3klauzul,
+Program zawiera 1 definicje relacji.
+Jest to relacja na/2.
+Definicja relacji na/2 sklada sie z
+3 klauzul, które s¹ faktami.
+*/
